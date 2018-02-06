752ec5cccbe36eb53a39969a5c95b5a8d59fe9ba
Tyler Reed <tyler.reed@ibm.com>
2018-02-06 16:32:35 -0500

    Added a GET request for a json file in directory

--- app.js
+++ app.js
@@ -7,23 +7,69 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
   $routeProvider
     .when('/', {
       templateUrl: '/views/form.html',
-      controller: 'formcontroller',
-      controller: 'settings'
+      controller: 'formcontroller'
     })
     .when('/404', {
       templateUrl: '/views/error404.html'
+    })
+    .when('/success', {
+      templateUrl: '/views/success.html'
     })
     .otherwise({
       redirectTo: '/404'
     });
 }]);
 
-app.controller('formcontroller', function($scope) {
-  // $scope.firstName = $scope.contact.firstName;
-  // $scope.lastName = $scope.contact.lastName;
-  // $scope.email = $scope.contact.email;
-  // $scope.password = $scope.contact.password;
-});
+app.controller('formcontroller', ['$scope', '$location', '$http',  function($scope, $location, $http) {
+  $scope.submit = function(data) {
+
+
+
+    $scope.myArray =
+      {
+        firstName: data.first,
+        lastName: data.lastName,
+        email: data.email,
+        password: data.password
+      };
+
+      console.log($scope.myArray);
+
+    // grabs the data and sets it to a json format
+    // console.log(angular.toJson($scope.myArray));
+
+    // $http.post('/success', JSON.stringify(data)).then(function(data) {
+    //   console.log(data);
+    // });
+
+    var config = {
+        headers : {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+    }
+
+    $http.post('/', JSON.stringify(data), config)
+      .then(
+        function (response) {
+          // success
+          console.log('SUCCESS');
+          console.log(response);
+        },
+        function (response) {
+          // failure
+          console.log('FAIL');
+          console.log(response);
+      });
+    $location.url('/success');
+  };
+
+  //need a server to run this code
+  $http.get('data/userinfo.json').then(function(data) {
+    $scope.myInfo = data.data;
+    console.log($scope.myInfo);
+  });
+}]);
 
 app.controller('settings', function($scope) {
   $scope.title = "Sign-in Form";
