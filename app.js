348274e8ec3c0ffcefbef0f9636ec1c131b9e653
Tyler Reed <tyler.reed@ibm.com>
2018-02-06 11:19:44 -0500

    Updated for routing

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+  //$locationProvider.html5Mode(true);
+
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/form.html',
+      controller: 'formcontroller',
+      controller: 'settings'
+    })
+    .when('/404', {
+      templateUrl: '/views/error404.html'
+    })
+    .otherwise({
+      redirectTo: '/404'
+    });
+}]);
 
 app.controller('formcontroller', function($scope) {
-  $scope.firstName = "Tyler";
+  // $scope.firstName = $scope.contact.firstName;
+  // $scope.lastName = $scope.contact.lastName;
+  // $scope.email = $scope.contact.email;
+  // $scope.password = $scope.contact.password;
 });
+
+app.controller('settings', function($scope) {
+  $scope.title = "Sign-in Form";
+  $scope.message = "Please Sign-in before proceeding";
+})
