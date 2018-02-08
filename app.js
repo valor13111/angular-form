6f6d752f2b5439452047cb2450a79590a709eb9a
Tyler Reed <tyler.reed@ibm.com>
2018-02-08 16:00:53 -0500

    Added a factory and another controller(submittedform)

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+app.factory("sharedService", function() {
+    // creating the variable for controllers
+    var dataObj = {};
 
-  //$locationProvider.html5Mode(true);
+    return {
+      setData: function(data) {
+        dataObj.firstName = data.firstName;
+        dataObj.lastName = data.lastName;
+        dataObj.email = data.email;
+      },
+      getData: function(data) {
+        return dataObj;
+      }
+    };
+});
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
   $routeProvider
     .when('/', {
@@ -13,65 +27,51 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
       templateUrl: '/views/error404.html'
     })
     .when('/success', {
-      templateUrl: '/views/success.html'
+      templateUrl: '/views/success.html',
+      controller: 'submittedForm'
     })
     .otherwise({
       redirectTo: '/404'
     });
+
 }]);
 
-app.controller('formcontroller', ['$scope', '$location', '$http',  function($scope, $location, $http) {
-  $scope.submit = function(data) {
+// CONTROLLER: the form controller, which is used to set the data in the factory service
+// this injects the formcontroller function into the app
+app.controller('formcontroller', ['$scope', '$location', '$http', 'sharedService', formcontroller]);
 
+// CONTROLLER: for '/success' view, after the form has been submitted
+// this injects the submittedform (controller) into the app
+app.controller('submittedForm', ['$scope', 'sharedService', submittedForm]);
 
-
-    $scope.myArray =
-      {
-        firstName: data.first,
-        lastName: data.lastName,
-        email: data.email,
-        password: data.password
-      };
-
-      console.log($scope.myArray);
-
-    // grabs the data and sets it to a json format
-    // console.log(angular.toJson($scope.myArray));
-
-    // $http.post('/success', JSON.stringify(data)).then(function(data) {
-    //   console.log(data);
-    // });
-
-    var config = {
-        headers : {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-    }
-
-    $http.post('/', JSON.stringify(data), config)
-      .then(
-        function (response) {
-          // success
-          console.log('SUCCESS');
-          console.log(response);
-        },
-        function (response) {
-          // failure
-          console.log('FAIL');
-          console.log(response);
-      });
-    $location.url('/success');
-  };
-
-  //need a server to run this code
-  $http.get('data/userinfo.json').then(function(data) {
-    $scope.myInfo = data.data;
-    console.log($scope.myInfo);
-  });
-}]);
-
+// CONTROLLER: for the form view, just practice to see if it works, sets the title and message
 app.controller('settings', function($scope) {
   $scope.title = "Sign-in Form";
   $scope.message = "Please Sign-in before proceeding";
-})
+});
+
+// function to submit the data
+function formcontroller($scope, $location, $http, sharedService) {
+  $scope.submit = function(data) {
+
+    sharedService.setData(data);
+    $location.url('/success');
+
+  };
+
+  // retrieves the data from the userinfo.json file
+  // $http.get('data/userinfo.json').then(function(data) {
+  //   $scope.myInfo = data.data;
+  //   console.log($scope.myInfo);
+  // });
+};
+
+function submittedForm($scope, sharedService) {
+
+  $scope.user = sharedService.getData();
+  console.log($scope.user.firstName);
+  console.log($scope.user.lastName);
+  console.log($scope.user.email);
+  console.log($scope.user.password);
+
+};
